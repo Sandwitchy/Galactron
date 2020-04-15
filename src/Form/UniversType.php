81702226db4b2c62014179bb4d85413a75759e63
Andréa <sandwitchy@xperidia.com>
2020-04-15 14:59:59 +0200

    Update js import

--- src/Form/UniversType.php
+++ src/Form/UniversType.php
@@ -12,7 +12,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 
-class UniversCreateType extends AbstractType
+class UniversType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
